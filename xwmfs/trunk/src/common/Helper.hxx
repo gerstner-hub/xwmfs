048f41f9326d989eb9d4c78464a78d60e96f360b
gerrit <gerrit@5c8ddfa2-8614-4b58-9c3b-211890f98cb0>
2017-01-12 21:03:36 +0000

    added strip() function for STL strings

--- xwmfs/trunk/src/common/Helper.hxx
+++ xwmfs/trunk/src/common/Helper.hxx
@@ -9,6 +9,13 @@ namespace xwmfs
 
 //! returns an all lower case version of \c s
 std::string tolower(const std::string &s);
+void strip(std::string &s);
+inline std::string stripped(const std::string &s)
+{
+	auto ret(s);
+	strip(ret);
+	return ret;
+}
 
 } // end ns
 
