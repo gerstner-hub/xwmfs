369fde0fa6cdc63623a26d161b7f8b5ae5a7de96
Matthias Gerstner <Matthias.Gerstner@nefkom.net>
2022-05-04 18:46:11 +0200

    Mutex.cxx: fix invalid stack access in DEBUG_MUTEX case
    gcc major version 12 issues the following warning:
    
    ```
    common/Mutex.cxx: In constructor ‘xwmfs::Mutex::Mutex()’:
    common/Mutex.cxx:48:71: error: dangling pointer ‘attr’ to ‘debug_attr’ may be used [-Werror=dangling-pointer=]
       48 |                 const int attr_destr_res = ::pthread_mutexattr_destroy(
          |                                            ~~~~~~~~~~~~~~~~~~~~~~~~~~~^
       49 |                         attr
          |                         ~~~~
       50 |                 );
          |                 ~
    common/Mutex.cxx:18:39: note: ‘debug_attr’ declared here
       18 |                 ::pthread_mutexattr_t debug_attr;
          |                                       ^~~~~~~~~~
    common/Mutex.cxx:39:33: error: using dangling pointer ‘attr’ to ‘debug_attr’ [-Werror=dangling-pointer=]
       39 |         if( ::pthread_mutex_init(&m_pmutex, attr) != 0 )
          |             ~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~
    common/Mutex.cxx:18:39: note: ‘debug_attr’ declared here
       18 |                 ::pthread_mutexattr_t debug_attr;
          |                                       ^~~~~~~~~~
    ```
    
    and indeed the `pthread_mutexattr_destroy` references the already
    out-of-scope `debug_attr`. It looks like this didn't show any symptoms
    up to now, since no new variables are put onto the stack at this time.
    
    To make things proper use a globally initialized attribute object
    instead. Cleanup is a bit hairy, use atexit() for now to clean it up. In
    fuse context cleanup is a bit hairy anyway.
    
    Also see Gentoo bug:
    
    https://bugs.gentoo.org/841020

--- src/common/Mutex.cxx
+++ src/common/Mutex.cxx
@@ -1,54 +1,73 @@
+#include <stdlib.h>
+
 #include "common/Mutex.hxx"
 
 namespace xwmfs
 {
 
+namespace {
+
+::pthread_mutexattr_t debug_attr;
+bool debug_attr_initialized = false;
+
 #ifndef NDEBUG
-static const bool DEBUG_MUTEX = true;
+constexpr bool DEBUG_MUTEX = true;
 #else
-static const bool DEBUG_MUTEX = false;
+constexpr bool DEBUG_MUTEX = false;
 #endif
+
+void cleanupDebug() {
+	if (!DEBUG_MUTEX)
+		return;
+	else if (!debug_attr_initialized)
+		return;
+
+	const int attr_destr_res = ::pthread_mutexattr_destroy(
+		&debug_attr
+	);
+	assert( ! attr_destr_res );
+	debug_attr_initialized = false;
+}
+
+void checkInitDebug() {
+	if (!DEBUG_MUTEX)
+		return;
+	else if (debug_attr_initialized)
+		return;
+
+	if( ::pthread_mutexattr_init(&debug_attr) != 0 )
+	{
+		xwmfs_throw(
+			xwmfs::SystemException("Error creating debug mutex attribute")
+		);
+	}
+
+	if( ::pthread_mutexattr_settype(
+		&debug_attr, PTHREAD_MUTEX_ERRORCHECK
+		) != 0 )
+	{
+		xwmfs_throw(
+			xwmfs::SystemException("Error setting debug mutex type")
+		);
+	}
+
+	atexit(cleanupDebug);
+	debug_attr_initialized = true;
+}
+
+} // end anon ns
 
 Mutex::Mutex()
 {
-	::pthread_mutexattr_t* attr = nullptr;
+	checkInitDebug();
+	::pthread_mutexattr_t* attr = DEBUG_MUTEX ? &debug_attr : nullptr;
 
-	if( DEBUG_MUTEX )
-	{
-		::pthread_mutexattr_t debug_attr;
-		if( ::pthread_mutexattr_init(&debug_attr) != 0 )
-		{
-			xwmfs_throw(
-				xwmfs::SystemException("Error creating debug mutex attribute")
-			);
-		}
-
-		if( ::pthread_mutexattr_settype(
-			&debug_attr, PTHREAD_MUTEX_ERRORCHECK
-			) != 0 )
-		{
-			xwmfs_throw(
-				xwmfs::SystemException("Error setting debug mutex type")
-			);
-		}
-
-
-		attr = &debug_attr;
-	}
 
 	if( ::pthread_mutex_init(&m_pmutex, attr) != 0 )
 	{
 		xwmfs_throw(
 			xwmfs::SystemException("Error creating mutex" )
 		);
-	}
-
-	if( DEBUG_MUTEX )
-	{
-		const int attr_destr_res = ::pthread_mutexattr_destroy(
-			attr
-		);
-		assert( ! attr_destr_res );
 	}
 }
 
